20988aaa6d8647fcf01e7f0f2759440c7f6e58d2
Sachkov <sachkovkrl@gmail.com>
2026-09-07 11:19:11 +0300

    chore: synchronize intake replay protection

--- .github/scripts/tracker_event.py
+++ .github/scripts/tracker_event.py
@@ -31,8 +31,6 @@ def arguments(event_name, event, repository, enabled, requested_apply=False, req
     elif event_name in {'issues', 'pull_request_target'}:
         item = event['issue' if event_name == 'issues' else 'pull_request']
         args += ['--issue', f"{repository}#{int(item['number'])}"]
-        if event.get('action') == 'opened':
-            args += ['--allow-add']
         if enabled:
             args += ['--apply']
     else:
